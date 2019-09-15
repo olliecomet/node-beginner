ebe0619f225f668777a5e3dbecb5be1d6e9740c7
ollie <leyechi@gmail.com>
2019-09-15 02:38:29 -0700

    route function

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const url = require('url');
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
-        let pathname = url.parse(request.url);
+        let pathname = url.parse(request.url).pathname;
         console.log(`Request for ${pathname} recieved.`);
+
+        route(pathname);
+
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('Hello World');
         response.end();
@@ -15,6 +18,19 @@ function start() {
 }
 
 exports.start = start;
+
+// function start() {
+    // function onRequest(request, response) {
+        // let pathname = url.parse(request.url);
+        // console.log(`Request for ${pathname} recieved.`);
+        // response.writeHead(200, {'Content-Type': 'text/plain'});
+        // response.write('Hello World');
+        // response.end();
+    // }
+// 
+    // http.createServer(onRequest).listen(8888);
+    // console.log('Server has started.');
+// }
 
 // function start() {
     // function onRequest(request, response) {
