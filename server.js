af13998035bda0037a223700be89fc6ba1d261a2
ollie <leyechi@gmail.com>
2019-09-15 01:49:52 -0700

    no HTTP req, uncalled callback

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const http = require('http');
 
-http.createServer(function(request, response) {
+function onRequest(request, response) {
+    console.log('Request recieved.');
     response.writeHead(200, {'content-type': 'text/plain'});
     response.write('Hello World');
     response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('Server has started.');
+
+// http.createServer(function(request, response) {
+    // response.writeHead(200, {'content-type': 'text/plain'});
+    // response.write('Hello World');
+    // response.end();
+// }).listen(8888);
