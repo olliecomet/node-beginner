ff7055cf7c6ba020a699082b761e11d728115694
ollie <leyechi@gmail.com>
2019-09-16 03:03:35 -0700

    blocking start opperation

--- request-handlers.js
+++ request-handlers.js
@@ -1,15 +1,32 @@
 function start() {
-    console.log(`Request handler 'start' was called.`);
+    console.log(`Request handler 'start' wasa called.`);
+
+    function sleep(milliseconds) {
+        let startTime = new Date().getTime();
+        while(new Date().getTime() < startTime + milliseconds);
+    }
+
+    sleep(10000);
     return 'Hello Start';
 }
 
 function upload() {
-    console.log(`Request handler 'upload' was called.`);
+    console.log(`Request handler for 'upload' was called.`)
     return 'Hello Upload';
 }
 
 exports.start = start;
 exports.upload = upload;
+
+// function start() {
+    // console.log(`Request handler 'start' was called.`);
+    // return 'Hello Start';
+// }
+// 
+// function upload() {
+    // console.log(`Request handler 'upload' was called.`);
+    // return 'Hello Upload';
+// }
 
 // function start() {
     // console.log(`Request handler 'start' was called.`);
