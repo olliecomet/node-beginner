eac8a6cbf3536c8e85bf488757713c2a68c62d6c
ollie <leyechi@gmail.com>
2019-09-15 20:28:59 -0700

    handle object

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const server = require('./server');
 const router = require('./router');
+const requestHandlers = require('./request-handlers');
 
-server.start(router.route);
+let handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+
+server.start(router.route, handle);
