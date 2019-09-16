ce1b9ad3b9318a6f267d74d02cb37afecd2a6584
ollie <leyechi@gmail.com>
2019-09-16 03:19:40 -0700

    empty exec

--- request-handlers.js
+++ request-handlers.js
@@ -1,22 +1,40 @@
+const exec = require('child_process').exec;
+
 function start() {
-    console.log(`Request handler 'start' wasa called.`);
+    console.log(`Request handler 'start' was called`);
+    let content = 'empty';
 
-    function sleep(milliseconds) {
-        let startTime = new Date().getTime();
-        while(new Date().getTime() < startTime + milliseconds);
-    }
+    exec('ls -lah', function (error, stdout, stderr) {
+        content = stdout;
+    });
 
-    sleep(10000);
-    return 'Hello Start';
+    return content;
 }
 
 function upload() {
-    console.log(`Request handler for 'upload' was called.`)
+    console.log(`Request handler 'upload' was called`);
     return 'Hello Upload';
 }
 
 exports.start = start;
 exports.upload = upload;
+
+// function start() {
+//     console.log(`Request handler 'start' wasa called.`);
+
+//     function sleep(milliseconds) {
+//         let startTime = new Date().getTime();
+//         while(new Date().getTime() < startTime + milliseconds);
+//     }
+
+//     sleep(10000);
+//     return 'Hello Start';
+// }
+
+// function upload() {
+//     console.log(`Request handler for 'upload' was called.`)
+//     return 'Hello Upload';
+// }
 
 // function start() {
     // console.log(`Request handler 'start' was called.`);
@@ -27,6 +45,9 @@ exports.upload = upload;
     // console.log(`Request handler 'upload' was called.`);
     // return 'Hello Upload';
 // }
+
+
+
 
 // function start() {
     // console.log(`Request handler 'start' was called.`);
